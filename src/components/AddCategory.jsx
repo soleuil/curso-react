0b5658310d4fda3394fb5316395750a5a0131cbb
soleuil <pedrotmartinez@gmail.com>
2022-10-01 01:08:21 +0200

    added gitignore

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 export const AddCategory = ({ onNewCategory }) => {
 
-    const [ inputValue, setInputValue ] = useState('One Punch')
+    const [ inputValue, setInputValue ] = useState('')
 
     const onInputChange = ({ target }) => {
         setInputValue( target.value );
