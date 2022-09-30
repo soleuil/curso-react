cf12bd4690acfe15b9397759509041d10cfbb533
soleuil <pedrotmartinez@gmail.com>
2022-10-01 01:11:01 +0200

    finished 07-heroes-spa

---
+++ src/heroes/pages/MarvelPage.jsx
@@ -0,0 +1,14 @@
+import { HeroList } from "../components"
+
+
+export const MarvelPage = () => {
+  return (
+    <>
+      <h1>MarvelPage</h1>
+      <hr />
+
+      <HeroList publisher={'Marvel Comics'} />
+    </>
+    
+  )
+}
