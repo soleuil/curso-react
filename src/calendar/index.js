26262689b29d36b5340948cda4772ebdf8e57dd1
soleuil <pedrotmartinez@gmail.com>
2022-10-06 00:50:17 +0200

    added modal and datePicker

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -3,3 +3,4 @@
 export * from './pages/CalendarPage';
 export * from './components/Navbar';
 export * from './components/CalendarEvent';
+export * from './components/CalendarModal';
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,10 @@
+import { useState } from 'react';
 import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { addHours } from 'date-fns';
 
-import { Navbar, CalendarEvent } from '../';
+import { Navbar, CalendarEvent, CalendarModal } from '../';
 import { localizer, getMessagesES } from '../../helpers';
 
 
@@ -21,6 +22,8 @@ const events = [{
 
 export const CalendarPage = () => {
 
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
+
   const eventStyleGetter = ( event, start, end, isSelected ) => {
 
     const style = {
@@ -33,8 +36,20 @@ export const CalendarPage = () => {
     return {
       style
     }
-
   }
+
+  const onDoubleClick = ( event ) => {
+    console.log({ doubleClick: event });
+  } 
+
+  const onSelect = ( event ) => {
+    console.log({ click: event });
+  } 
+
+  const onViewChange= ( event ) => {
+    localStorage.setItem('lastView', event );
+    setLastView( event )
+  } 
 
   return (
     <>
@@ -43,7 +58,8 @@ export const CalendarPage = () => {
       <Calendar
         culture='es'
         localizer={ localizer }
-        events={events}
+        events={ events }
+        defaultView={ lastView }
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc( 100vh - 80px )' }}
@@ -52,8 +68,12 @@ export const CalendarPage = () => {
         components={{
           event: CalendarEvent
         }}
+        onDoubleClickEvent={ onDoubleClick }
+        onSelectEvent={ onSelect }
+        onView={ onViewChange }
       />
 
+        <CalendarModal />
 
     </>
   )
